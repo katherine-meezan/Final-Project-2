a140a14f6412ea416a0284970d1fb47272621d02
Cara Razma <cara.raz1@lvjusd.k12.ca.us>
2021-11-04 14:38:46 -0700

    items

--- Player.java
+++ Player.java
@@ -1,10 +1,11 @@
-
+import java.util.ArrayList;
 
 public class Player
 {
    private int health;
    private int xp;
    private int power;
+   ArrayList<String> items = new ArrayList<String>();
    
    public Player(){
        this.health = 100;
@@ -18,6 +19,8 @@ public class Player
        this.power = power;
     }
     
+   // getters and setters
+   ///////////////////////////////////////////////////////////
    public int getHealth(){
        return this.health;
     }
@@ -47,6 +50,52 @@ public class Player
    public void addPower(int morePower){
        this.power += morePower;
     }
+    
+    
+    
+   // Items
+   ///////////////////////////////////////////////////////////
+   public ArrayList<String> returnItems(){
+       return items;
+   }
+   
+   public void addItem(String Item){
+       items.add(Item);
+   }
+   
+   public void addItem(String Item, int amount){
+       for (int i = 0; i > amount; i++){
+           items.add(Item);
+        }
+   }
+   
+   public void removeItem(String Item){
+       items.remove(items.indexOf(Item));
+   }
+   
+   // Gameplay
+   ///////////////////////////////////////////////////////////
+   
+      public void corner(String answer){
+       if (answer.equals("1")){
+           print("A zombie springs out! Oh no!");
+           // combat();
+       } else if (answer.equals("2")){
+           print("You found two bottles with strange red liquid");
+           addItem("health poition", 2);
+       } else if (answer.equals("3")){
+           print("You found a key! What's it for...");
+           addItem("key");
+       } else if(answer.equals("4")){
+           print("A zombie springs out! Oh no!");
+       } else if(answer.equals("door")){
+           print("The door is locked");
+       }
+   }
+   
+      public static void print(String s){
+       System.out.println(s);
+   }
 }
 
 
